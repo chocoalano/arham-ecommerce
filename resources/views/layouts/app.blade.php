729697ebac1132ab8ecfac614e08c11a00c98946
chocoalano <alangentina95@gmail.com>
2025-11-04 09:39:59 +0700

    update title in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>@yield('title', 'Pataku - Furniture eCommerce Bootstrap5 Template')</title>
+    <title>@yield('title', 'Arham - Moslem wear')</title>
     <meta name="description" content="@yield('meta_description', '')">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
